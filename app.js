259c7608236c41689e40fc4599fcaf1693020baa
Lutfu24 <lutfuhm@code.edu.az>
2025-08-03 00:48:31 +0400

    dark mode

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ function scrollCards() {
 }
 
 function getfilData(region = "") {
-  let filData = [];
   if (!region) {
     return (filData = data.filter((item) => item.name !== "Armenia"));
   } else {
@@ -51,7 +50,7 @@ function getData(region = "") {
                             href="./detail.html?aplhaCode=${
                               findedElement.alpha3Code
                             }"
-                            class="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-black dark:text-gray-50"
+                            class="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-black text-white dark:bg-red-900 dark:text-gray-50"
                             fdprocessedid="4kvp"
                             >
                             Read more
@@ -81,7 +80,7 @@ function getData(region = "") {
                             <a
                             target="_blank"
                             href="./detail.html?aplhaCode=${item.alpha3Code}"
-                            class="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-black dark:text-gray-50"
+                            class="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-black text-white dark:bg-red-900 dark:text-gray-50"
                             fdprocessedid="4kvp"
                             >
                             Read more
@@ -93,10 +92,8 @@ function getData(region = "") {
 }
 getData();
 
-let regionsArr = [];
 function getRegion() {
   let regions = [...new Set(data.map((item) => item.region))];
-  regionsArr = regions;
   regions.forEach((item) => {
     list.innerHTML += `<li class="flex">
                         <button
@@ -124,3 +121,64 @@ function showMore() {
   }
   getData();
 }
+const drkDiv = document.getElementById("dark-mode-div");
+drkDiv.innerHTML = `<i class="fa-solid fa-sun fa-2xl" style="color: #f5cb32"></i>`;
+
+function changeDarkMode() {
+  document.documentElement.classList.toggle("dark");
+
+  if (
+    drkDiv.innerHTML ==
+    `<i class="fa-solid fa-sun fa-2xl" style="color: #f5cb32"></i>`
+  ) {
+    drkDiv.innerHTML = `<i class="fa-solid fa-moon fa-2xl" style="color: #000"></i>`;
+  } else if (
+    drkDiv.innerHTML ==
+    `<i class="fa-solid fa-moon fa-2xl" style="color: #000"></i>`
+  ) {
+    drkDiv.innerHTML = `<i class="fa-solid fa-sun fa-2xl" style="color: #f5cb32"></i>`;
+  }
+}
+
+drkDiv.addEventListener("click", changeDarkMode);
+
+const input = document.getElementById("inputS");
+
+function searchCountry() {
+  html = "";
+
+  getfilData()
+    .filter((item) =>
+      item.name.toUpperCase().startsWith(input.value.toUpperCase())
+    )
+    .forEach((item) => {
+      html += `<div
+                        class="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800"
+                        >
+                        <img
+                            src=${item.flag}
+                            alt=""
+                            class="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
+                        />
+                        <div class="flex flex-col justify-between p-6 space-y-8">
+                            <div class="space-y-2">
+                            <span class="block text-xs font-medium tracking-widest uppercase dark:text-black">${item.region}</span>
+                            <h2 class="text-3xl font-semibold dark:text-green-800 tracking-wide truncate">
+                                ${item.name}, ${item.capital}
+                            </h2>
+                            </div>
+                            <a
+                            target="_blank"
+                            href="./detail.html?aplhaCode=${item.alpha3Code}"
+                            class="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-black text-white dark:bg-red-900 dark:text-gray-50"
+                            fdprocessedid="4kvp"
+                            >
+                            Read more
+                            </a>
+                        </div>
+                        </div>`;
+    });
+  container.innerHTML = html;
+}
+
+input.addEventListener("input", searchCountry);
