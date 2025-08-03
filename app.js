8ff7572ce22acfa58b25ff681be35b6fec1fb7d2
Lutfu24 <lutfuhm@code.edu.az>
2025-08-04 00:19:05 +0400

    search

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const container = document.getElementById("container");
 const list = document.getElementById("list");
 const showBtn = document.getElementById("showBtn");
+const input = document.getElementById("inputS");
+const drkDiv = document.getElementById("dark-mode-div");
 
 let count = 12;
 let html = "";
@@ -18,7 +20,7 @@ function getfilData(region = "") {
     ));
   }
 }
-function getData(region = "") {
+function getData(region = "", elements = []) {
   head.innerHTML = "";
   let randomNum = Math.floor(
     getfilData(region).length - Math.random() * getfilData(region).length
@@ -59,10 +61,9 @@ function getData(region = "") {
                         </div>`;
 
   html = "";
-  getfilData(region)
-    .slice(0, count)
-    .forEach((item) => {
-      html += `<div
+  elements.length > 0
+    ? elements.slice(0, count).forEach((item) => {
+        html += `<div
                         class="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800"
                         >
                         <img
@@ -87,7 +88,36 @@ function getData(region = "") {
                             </a>
                         </div>
                         </div>`;
-    });
+      })
+    : getfilData(region)
+        .slice(0, count)
+        .forEach((item) => {
+          html += `<div
+                        class="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800"
+                        >
+                        <img
+                            src=${item.flag}
+                            alt=""
+                            class="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
+                        />
+                        <div class="flex flex-col justify-between p-6 space-y-8">
+                            <div class="space-y-2">
+                            <span class="block text-xs font-medium tracking-widest uppercase dark:text-black">${item.region}</span>
+                            <h2 class="text-3xl font-semibold dark:text-green-800 tracking-wide truncate">
+                                ${item.name}, ${item.capital}
+                            </h2>
+                            </div>
+                            <a
+                            target="_blank"
+                            href="./detail.html?aplhaCode=${item.alpha3Code}"
+                            class="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-black text-white dark:bg-red-900 dark:text-gray-50"
+                            fdprocessedid="4kvp"
+                            >
+                            Read more
+                            </a>
+                        </div>
+                        </div>`;
+        });
   container.innerHTML = html;
 }
 getData();
@@ -107,7 +137,9 @@ function getRegion() {
 }
 getRegion();
 
+let regionsArr = [];
 function getFilteredData(reg) {
+  regionsArr.push(reg);
   btnshow.style.display = "none";
   count = 50;
   html = "";
@@ -121,64 +153,39 @@ function showMore() {
   }
   getData();
 }
-const drkDiv = document.getElementById("dark-mode-div");
 drkDiv.innerHTML = `<i class="fa-solid fa-sun fa-2xl" style="color: #f5cb32"></i>`;
 
+let flag = false;
 function changeDarkMode() {
+  flag = !flag;
+  drkDiv.innerHTML = flag
+    ? `<i class="fa-solid fa-moon fa-2xl" style="color: #000"></i>`
+    : `<i class="fa-solid fa-sun fa-2xl" style="color: #f5cb32"></i>`;
   document.documentElement.classList.toggle("dark");
-
-  if (
-    drkDiv.innerHTML ==
-    `<i class="fa-solid fa-sun fa-2xl" style="color: #f5cb32"></i>`
-  ) {
-    drkDiv.innerHTML = `<i class="fa-solid fa-moon fa-2xl" style="color: #000"></i>`;
-  } else if (
-    drkDiv.innerHTML ==
-    `<i class="fa-solid fa-moon fa-2xl" style="color: #000"></i>`
-  ) {
-    drkDiv.innerHTML = `<i class="fa-solid fa-sun fa-2xl" style="color: #f5cb32"></i>`;
-  }
 }
 
 drkDiv.addEventListener("click", changeDarkMode);
 
-const input = document.getElementById("inputS");
-
 function searchCountry() {
-  html = "";
+  count = 50;
+  let findElem = [];
+  if (regionsArr.length > 0) {
+    console.log(regionsArr[regionsArr.length - 1]);
+    findElem = data.filter(
+      (item) =>
+        item.name.toUpperCase().startsWith(input.value.toUpperCase()) &&
+        item.name !== "Armenia" &&
+        item.region == regionsArr[regionsArr.length - 1]
+    );
+  } else {
+    findElem = data.filter(
+      (item) =>
+        item.name.toUpperCase().startsWith(input.value.toUpperCase()) &&
+        item.name !== "Armenia"
+    );
+  }
 
-  getfilData()
-    .filter((item) =>
-      item.name.toUpperCase().startsWith(input.value.toUpperCase())
-    )
-    .forEach((item) => {
-      html += `<div
-                        class="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800"
-                        >
-                        <img
-                            src=${item.flag}
-                            alt=""
-                            class="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
-                        />
-                        <div class="flex flex-col justify-between p-6 space-y-8">
-                            <div class="space-y-2">
-                            <span class="block text-xs font-medium tracking-widest uppercase dark:text-black">${item.region}</span>
-                            <h2 class="text-3xl font-semibold dark:text-green-800 tracking-wide truncate">
-                                ${item.name}, ${item.capital}
-                            </h2>
-                            </div>
-                            <a
-                            target="_blank"
-                            href="./detail.html?aplhaCode=${item.alpha3Code}"
-                            class="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-black text-white dark:bg-red-900 dark:text-gray-50"
-                            fdprocessedid="4kvp"
-                            >
-                            Read more
-                            </a>
-                        </div>
-                        </div>`;
-    });
-  container.innerHTML = html;
+  getData("", findElem);
 }
 
 input.addEventListener("input", searchCountry);
